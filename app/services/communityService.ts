4a056034b3992b04006068454e296fec216df90f
Ranjith <ranjith.r@communn.io>
2025-05-05 14:23:21 +0530

    Updated code

--- app/services/communityService.ts
+++ app/services/communityService.ts
@@ -59,6 +59,7 @@ export async function getCommunityData(hostOrSubdomain: string): Promise<{ commu
         vision: '',
         mission: '',
         template: 'default',
+        community: ''
       },
     };
   }
